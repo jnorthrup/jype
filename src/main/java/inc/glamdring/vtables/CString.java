d7832f8c84afc84e5cbda314cf9af923d39826ba
jim <jim@vsiwest.com>
2016-01-06 00:35:16 -0800

    more ocd tweaks

--- src/main/java/inc/glamdring/vtables/CString.java
+++ src/main/java/inc/glamdring/vtables/CString.java
@@ -10,31 +10,33 @@ import static bbcursive.std.bb;
 import static bbcursive.std.str;
 
 /**
- * this class reads a null terminated string. the null is not returned
+ * this class reads a null terminated string. the null is not included
  */
 
-public class CString extends _reifier<String> {
-    @Override
-    public String apply(_ptr ptr) {
-        ByteBuffer b = bb(ptr.core(ptr),mark,slice);
-        while(b.hasRemaining()&&b.get()>0);
-        return str(b.flip());
-    }
-
-    private final My_mutator mutator = new My_mutator();
-
+public class CString {
+    private final _mutator$ mutator = new _mutator$();
+    private final _reifier$ reifier = new _reifier$();
     public _mutator<String> getMutator() {
         return mutator;
     }
+    public _reifier<String> getReifier() { return reifier; }
 
-    private class My_mutator extends _mutator<String> {
+    private class _mutator$ extends _mutator<String> {
         @Override
         public _ptr apply(String s) {
 
             ByteBufferContext context = getContext();
             _ptr at = context.at();
-            bb(std.cat(bb(at.core(), mark), StandardCharsets.UTF_8.encode(s) ),duplicate,reset,slice,debug);//wordy, but not doing much
+            bb(std.cat(bb(at.core(), mark), StandardCharsets.UTF_8.encode(s)), duplicate, reset, slice, debug);//wordy, but not doing much
             return getContext().at();
+        }
+    }
+    private class _reifier$ implements _reifier<String> {
+        @Override
+        public String apply(_ptr ptr) {
+            ByteBuffer b = bb(ptr.core(ptr), mark, slice);
+            while (b.hasRemaining() && b.get() > 0) ;
+            return str(b.flip());
         }
     }
 }
